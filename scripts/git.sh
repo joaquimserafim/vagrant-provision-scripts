4b49fa42ef6a92f4851e61f2cda31a0a4c193951
quim <joaquim.serafim@gmail.com>
2014-12-22 16:51:46 +0000

    update git script

--- scripts/git.sh
+++ scripts/git.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
-echo "#install git"
+OP="install git"
+
+echo "# $OP"
+
+START=$(date +%s)
 
 apt-get install -y git 1> /dev/null 2>&1
 git --version
+
+END=$(date +%s)
+DIFF=$(( $END - $START ))
+echo "´$OP´ took $DIFF seconds"
