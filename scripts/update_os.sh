3cd7aca341c5bcc6275739c4c9c76df4b771d505
quim <joaquim.serafim@gmail.com>
2014-12-22 16:11:55 +0000

    check time spent in the tasks

--- scripts/update_os.sh
+++ scripts/update_os.sh
@@ -2,6 +2,12 @@
 
 echo "#update OS"
 
+START=$(date +%s)
+
 apt-get update 1> /dev/null 2>&1
 apt-get upgrade -y 1> /dev/null 2>&1
 apt-get install -y language-pack-en 1> /dev/null 2>&1
+
+END=$(date +%s)
+DIFF=$(( $END - $START ))
+echo "It took $DIFF seconds"
