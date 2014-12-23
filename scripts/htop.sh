d8f1748a843aada86a891b84f3f064006b2b6f18
Joaquim Serafim <joaquimserafim@users.noreply.github.com>
2014-12-23 17:16:55 +0000

    Update htop.sh

--- scripts/htop.sh
+++ scripts/htop.sh
@@ -1,14 +1,4 @@
 #!/bin/bash
 
-OP="install htop"
-
-echo "# $OP"
-
-START=$(date +%s)
-
 apt-get install -y htop 1> /dev/null 2>&1
 htop --version | sed -n 1p
-
-END=$(date +%s)
-DIFF=$(( $END - $START ))
-echo "´$OP´ took $DIFF seconds"
