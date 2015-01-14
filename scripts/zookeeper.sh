133d1e73ac3d73e9d8b27675764213515b55092f
Joaquim Serafim <joaquimserafim@users.noreply.github.com>
2015-01-14 23:46:21 +0000

    Update zookeeper.sh

--- scripts/zookeeper.sh
+++ scripts/zookeeper.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-sudo apt-get update  1> /dev/null 2>&1
-sudo apt-get install -y zookeeper 1> /dev/null 2>&1
-sudo apt-get install -y zookeeper-bin 1> /dev/null 2>&1
-sudo apt-get install -y zookeeperd 1> /dev/null 2>&1
+apt-get update  1> /dev/null 2>&1
+apt-get install -y zookeeper 1> /dev/null 2>&1
+apt-get install -y zookeeper-bin 1> /dev/null 2>&1
+apt-get install -y zookeeperd 1> /dev/null 2>&1
 # check zookeeper version
 echo srvr | nc localhost 2181 | sed -n 1p
