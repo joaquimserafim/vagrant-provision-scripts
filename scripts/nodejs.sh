c44efcad9d65e5ef84136785b1276e36022b80d6
Joaquim Serafim <joaquimserafim@users.noreply.github.com>
2015-02-02 14:58:50 +0000

    fix npm installation mode

--- scripts/nodejs.sh
+++ scripts/nodejs.sh
@@ -8,6 +8,9 @@ git clone -q https://github.com/tj/n.git /home/vagrant/n 1> /dev/null 2>&1
 cd /home/vagrant/n
 make install 1> /dev/null 2>&1
 n stable 1> /dev/null 2>&1
+
+echo "spin=false\nloglevel=http" > ~/.npmrc
+
 echo "n version `n --version`"
 echo "node version `node -v`"
 echo "npm version `npm --version`"
